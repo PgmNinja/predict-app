763694093656fcfc997aa23a5324f2e19df0ae89
niranjana <niranjana@qburst.com>
2022-02-05 06:15:30 +0530

    Hid secret key

--- prediction-app-backend/core/settings.py
+++ prediction-app-backend/core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@2t0pa*=(=dh76g%_yosscc=qc-=jg2e2%n%$e)o!bnf$6%lv4'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
