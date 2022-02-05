ae99b88c40ba40bbb5cebe072719da4a591a9a32
niranjana <niranjana@qburst.com>
2022-02-05 22:23:26 +0530

    modified entrypoint

--- prediction-app-backend/entrypoint.sh
+++ prediction-app-backend/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
 
-cd prediction-app-backend && python manage.py flush --no-input
-cd prediction-app-backend && python manage.py migrate
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
