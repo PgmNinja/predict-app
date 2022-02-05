ece6bc05c069cf62b8aa5fe5f7a7927bddd771de
niranjana <niranjana@qburst.com>
2022-02-05 23:30:29 +0530

    modified

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd prediction-app-backend && python manage.py makemigrations --no-input
-cd prediction-app-backend && python manage.py migrate --no-input
+python prediction-app-backend/manage.py makemigrations --no-input
+python prediction-app-backend/manage.py migrate --no-input
 
 
