1c9cee723db45ff3d0eb5e1fe358bd88569cc44b
niranjana <niranjana@qburst.com>
2022-02-13 15:15:21 +0530

    changed db

--- prediction-app-backend/core/settings/environments/local/local.py
+++ prediction-app-backend/core/settings/environments/local/local.py
@@ -10,13 +10,20 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0']
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         "NAME": env("SQL_DATABASE"),
+#         "USER": env("SQL_USER"),
+#         "PASSWORD": env("SQL_PASSWORD"),
+#         "HOST": env("SQL_HOST"),
+#         "PORT": env("SQL_PORT"),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "NAME": env("SQL_DATABASE"),
-        "USER": env("SQL_USER"),
-        "PASSWORD": env("SQL_PASSWORD"),
-        "HOST": env("SQL_HOST"),
-        "PORT": env("SQL_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
