f731c2738849156b15309be74bc2c711534f34e9
niranjana <niranjana@qburst.com>
2022-02-20 16:43:02 +0530

    modified project structure

---
+++ backend/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+until cd /app/prediction-app-backend
+do
+    echo "Waiting for server volume..."
+done
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+python manage.py runserver 0.0.0.0:$PORT
+
+exec "$@"
