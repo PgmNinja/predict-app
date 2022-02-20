959708f811819e4aaf120fd27db92cc6031efc47
niranjana <niranjana@qburst.com>
2022-02-20 18:47:24 +0530

    Modified staging env

--- backend/core/settings/environments/staging/staging.py
+++ backend/core/settings/environments/staging/staging.py
@@ -6,7 +6,21 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['epl-prediction-app.herokuapp.com', 'www.epl-prediction-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('PRODUCTION_HOST')]
+
+INSTALLED_APPS.extend(['whitenoise.runserver_nostatic'])
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, '../', 'frontend', 'build')]
+
+STATICFILES_DIRS = [os.path.join(
+    BASE_DIR, '../', 'frontend', 'build', 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, '../', 'frontend', 'build', 'root')
 
 DATABASES = {
     'default': {
